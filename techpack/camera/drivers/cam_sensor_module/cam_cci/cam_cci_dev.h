d47ba128db0ef0e7ad8e45affd5f5b036d644476
amackpro <prajwal.kumar008@gmail.com>
2024-03-08 13:13:21 +0530

    Revert "Merge 94757acf74557a6efeaee18c70cb049751137cdb on remote branch"
    This reverts commit 254df8da31dfa0ea0e6864374f4c9a5a06edb798.

--- techpack/camera/drivers/cam_sensor_module/cam_cci/cam_cci_dev.h
+++ techpack/camera/drivers/cam_sensor_module/cam_cci/cam_cci_dev.h
@@ -38,7 +38,7 @@
 #define CYCLES_PER_MICRO_SEC_DEFAULT 4915
 #define CCI_MAX_DELAY 1000000
 
-#define CCI_TIMEOUT msecs_to_jiffies(100)
+#define CCI_TIMEOUT msecs_to_jiffies(1500)
 
 #define NUM_MASTERS 2
 #define NUM_QUEUES 2
